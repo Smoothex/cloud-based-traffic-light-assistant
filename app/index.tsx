ac40bdfce15e9ded303b10fce7583260fadf2f5b
Georgi Kotsev <37122481+gogokotsev00@users.noreply.github.com>
2024-05-25 01:27:45 +0200

    Added search field with auto completion, removed strict null checks for now

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,54 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions, SafeAreaView } from "react-native";
 import { useState, useEffect, useRef } from "react";
 import * as Location from "expo-location";
-import MapView, { PROVIDER_GOOGLE, Region } from "react-native-maps";
+import MapView, { LatLng, Marker, PROVIDER_GOOGLE, Point, Region } from "react-native-maps";
+import { GooglePlaceDetail, GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+const screen = Dimensions.get('window');
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE_DELTA = 0.04;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const REGION_BERLIN = {
   latitude: 52.520008,
   longitude: 13.404954,
-  latitudeDelta: 2,
-  longitudeDelta: 2
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA
 }
 
 export default function App() {
   const [location, setLocation] = useState({} as Location.LocationObject);
-  const [errorMsg, setErrorMsg] = useState('');
+  const [destination, setDestination] = useState({latitude: REGION_BERLIN.latitude, longitude: REGION_BERLIN.longitude} as LatLng);
   const [mapMoved, setMapMoved] = useState(false);
   const mapRef = useRef<MapView>({} as MapView);
+
+   const onPressAddress = (details:GooglePlaceDetail) => {
+    setDestination({
+      latitude: details?.geometry?.location.lat,
+      longitude: details?.geometry?.location.lng,
+    });
+    if (details) {
+      moveToLocation(details?.geometry?.location.lat, details?.geometry?.location.lng);
+    }
+  };
+
+  async function moveToLocation(latitude:number, longitude:number) {
+    mapRef.current.animateToRegion(
+      {
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      2000,
+    );
+  };
 
   useEffect(() => {
     (async () => {
 
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
         return;
       }
 
@@ -31,42 +58,50 @@ export default function App() {
   }, []);
 
   const onRegionChange = (region: Region) => {
-		setMapMoved(true);
-	};
+    setMapMoved(true);
+  };
 
   return (
-    <View style={styles.container}>
-      <MapView provider={PROVIDER_GOOGLE} 
-        style={styles.map} 
-        initialRegion={REGION_BERLIN} 
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchContainer}>
+        <GooglePlacesAutocomplete
+          placeholder="Search your destination"
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            onPressAddress(details);
+          }}
+          query={{
+            key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+            language: 'en',
+          }}
+        />
+      </View>
+      <MapView
+        style={styles.map}
+        ref={mapRef}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={REGION_BERLIN}
         showsMyLocationButton={mapMoved}
         showsUserLocation
-        onRegionChangeComplete={onRegionChange}
-        ref={mapRef}
-      ></MapView>
-    </View>
+        onRegionChangeComplete={onRegionChange}>
+        {(destination.latitude && destination.longitude) && <Marker coordinate={destination} />}
+      </MapView>
+    </SafeAreaView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  bigHeader: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: 'red',
-  },
-  paragraph: {
-    fontSize: 16,
-    textAlign: 'center',
   },
   map: {
-    width: '100%',
-    height: '100%',
-  }
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 0,
+  },
+  searchContainer: {
+    zIndex: 1,
+    flex: 0.4,
+    marginRight: 60,
+  },
 });
